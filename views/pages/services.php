adcdc0604f2fc5ae1d11774aa7bd2e94b01be63a
Achyl Janot l'élève aux pensé seraine <lychal0411@gmail.com>
2025-11-25 11:38:13 +0100

    Test du serveur
    Tester d'afficher la page sur le serveur interne de MACHINULTIME

---
+++ views/pages/services.php
@@ -0,0 +1,6 @@
+<section>
+	<h1>Mentions Légales</h1>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam vero ea minima officiis quidem debitis, tempora repudiandae, eum numquam facilis est necessitatibus nobis iusto. Laboriosam earum sed quaerat vel quibusdam!
+	Ipsum doloribus officiis quasi neque eos eaque quibusdam quae. Illum cum commodi reiciendis a nihil, fugiat debitis temporibus magnam non voluptatibus autem nobis quisquam sequi rem praesentium dignissimos? Voluptatem, nulla?
+	Ipsam expedita itaque accusantium repellat, magni quas exercitationem officia consequatur ut porro praesentium labore tenetur quos qui. Illum dignissimos aperiam ab optio eum sed, dicta error. Quis consectetur voluptate facilis!</p>
+</section>

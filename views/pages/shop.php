481e4ad560e15a0facdf79b930869cb8355e7f1d
anthonin-tech <anthonin.chesneau08@gmail.com>
2025-11-25 11:56:53 +0100

    ajout du début de search

---
+++ views/pages/shop.php
@@ -0,0 +1,1 @@
+<?php include('views/partials/_search.php') ?>
---
+++ views/partials/_search.php
@@ -0,0 +1,11 @@
+<form name="fo" method="get" action="">
+    <input type="text" name="keywords"
+    placeholder="Mots-clés" />
+    <input type="submit" name="valider" value="Rechercher" />
+</form>
+<div id="resultats">
+    <div id="nbr"> 2 résultats trouvés</div>
+    <ol>
+        <li>Résultats 1</li>
+    </ol>
+</div>

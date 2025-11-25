9d1fb24512c02b90bfe1ef2779645fced6c45614
Made154 <pernotmahe33@gmail.com>
2025-11-25 20:40:50 +0100

    udpate index

--- index.php
+++ index.php
@@ -4,15 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Clarté Ornée</title>
-    <link
-    rel="stylesheet"
-    href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css">
     <link rel="stylesheet" href="assests/css/style.css">
 </head>
 <body>
     <?php include('views/partials/_navbar.php') ?>
     <main class="container">
-        <?php include ('views/pages/'. $_GET['pages'] .'.php') ?>
+         <?php include ('views/pages/'. $_GET['page'] .'.php') ?>
     </main>
     <?php include('views/partials/_footer.php') ?>
 </body>

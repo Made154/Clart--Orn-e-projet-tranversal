6395a8e7892a52004845fb189cb19cb7b519b1f7
Made154 <pernotmahe33@gmail.com>
2025-11-28 16:16:59 +0100

    udpate index css

--- index.php
+++ index.php
@@ -7,6 +7,10 @@
     <link rel="stylesheet" href="assests/css/app.css">
 </head>
 <body>
+    <div class="bloc">
+         <div class="triangle"></div>
+    </div>
+
     <?php include('views/partials/_navbar.php') ?>
     <main class="container">
     <?php

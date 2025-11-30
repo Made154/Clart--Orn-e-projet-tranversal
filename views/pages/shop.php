12d7d74256a4bcf06a9faf1ce4437012f5b2c017
anthonin-tech <anthonin.chesneau08@gmail.com>
2025-11-30 14:04:50 +0100

    v1 de la boutique

--- views/pages/shop.php
+++ views/pages/shop.php
@@ -1,1 +1,23 @@
 <?php include('views/partials/_search.php') ?>
+<?php for ($i = 0; $i < sizeof($article); $i++)?>
+
+<h2 class=name_category>Table</h2>
+
+<img src='<?= $article [$i]["illustration"] ?>' alt=''>
+
+<h2 class=name_category>Lampadaire</h2>
+
+
+<h2 class=name_category>Suspension</h2>
+
+
+<h2 class=name_category>Murale</h2>
+
+
+<h2 class=name_category>Bureau</h2>
+
+
+<h2 class=name_category>LED</h2>
+
+
+<h2 class=name_category>Enfants</h2>

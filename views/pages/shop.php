f8d5d6b86d9efdc462067778e8ceba6e14c43875
anthonin-tech <anthonin.chesneau08@gmail.com>
2025-12-04 10:02:33 +0100

    ajout des images de bureau

--- views/pages/shop.php
+++ views/pages/shop.php
@@ -1,44 +1,51 @@
 <?php include('views/partials/_search.php') ?>
 
     <h2 class=name_category>Table</h2>
-        <?php 
-            $id_category = 1;
-            include('models/item_display.php') 
-        ?>
-        
+        <div class=article>
+            <?php 
+                $id_category = 1;
+                include('models/item_display.php') 
+            ?>
+        </div>
     <h2 class=name_category>Lampadaire</h2>
-        <?php 
-            $id_category = 2;
-            include('models/item_display.php') 
-        ?>
-        
+        <div class=article>
+            <?php 
+                $id_category = 2;
+                include('models/item_display.php') 
+            ?>
+        </div>
     <h2 class=name_category>Suspension</h2>
+        <div class=article>
         <?php 
-            $id_category = 3;
-            include('models/item_display.php') 
-        ?>
-        
+                $id_category = 3;
+                include('models/item_display.php') 
+            ?>
+        </div>
     <h2 class=name_category>Murale</h2>
-        <?php 
-            $id_category = 4;
-            include('models/item_display.php') 
-        ?>
-        
+        <div class=article>
+            <?php 
+                $id_category = 4;
+                include('models/item_display.php') 
+            ?>
+        </div>
     <h2 class=name_category>Bureau</h2>
-        <?php 
-            $id_category = 5;
-            include('models/item_display.php') 
-        ?>
-        
+        <div class=article>
+            <?php 
+                $id_category = 5;
+                include('models/item_display.php') 
+            ?>
+        </div>
     <h2 class=name_category>LED</h2>
-        <?php 
-            $id_category = 6;
-            include('models/item_display.php') 
-        ?>
-        
+        <div class=article>
+            <?php 
+                $id_category = 6;
+                include('models/item_display.php') 
+            ?>
+        </div>
     <h2 class=name_category>Enfants</h2>
-        <?php 
-            $id_category = 7;
-            include('models/item_display.php') 
-        ?>
-        
+        <div class=article>
+            <?php 
+                $id_category = 7;
+                include('models/item_display.php') 
+            ?>
+        </div>

3e53c9a00b72e636d42d6d2d51151b81ea590f80
Made154 <pernotmahe33@gmail.com>
2025-12-10 10:39:45 +0100

    udpt

--- index.php
+++ index.php
@@ -5,10 +5,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Clarté Ornée</title>
     <link rel="stylesheet" href="assests/css/app.css">
+    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;600;800&display=swap" rel="stylesheet">
 </head>
-<body>
-    
+<body style="
+    background-image: url(https://ecoquartiervauban.fr/wp-content/uploads/2024/12/temp-imagejpg-4.jpg);
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+">
+
     <?php include('views/partials/_navbar.php') ?>
+    <?php include('views/partials/_section.php') ?>
+
+   
     <main class="container">
     <?php
         $page = $_GET['page'] ?? 'home';
---
+++ views/partials/_section.php
@@ -0,0 +1,6 @@
+<div class="square">
+    <nav class="Placement">
+	    <a href="index.php?page=home" class="Nom_boutique">Clarté Ornée</a>
+    </nav>
+    <?php include('views/partials/_search.php') ?>
+</div>

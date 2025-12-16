8566563e1c88d261b2aaba10370832b6d49a04fd
Made154 <pernotmahe33@gmail.com>
2025-12-16 17:24:06 +0100

    udpate

--- models/bdd.php
+++ models/bdd.php
@@ -1,6 +1,6 @@
 <?php
 
-$db = new PDO("mysql:host=localhost;dbname=clarte_ornee;charset=utf8", "root", "admin");
+$db = new PDO("mysql:host=localhost;dbname=clarte_ornee;charset=utf8", "root", "");
 $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 $sql = "SELECT * FROM article";
 $query = $db->query($sql);

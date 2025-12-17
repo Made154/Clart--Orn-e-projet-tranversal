7d49f8342bad9fa6142cb328c94eeae8b9231ded
anthonin-tech <anthonin.chesneau08@gmail.com>
2025-12-17 01:56:33 +0100

    modif css form

--- views/pages/update.php
+++ views/pages/update.php
@@ -2,78 +2,71 @@
 
 	<h1>Mettre à jour le produit <?= $article['name'] ?></h1>
 
-	<form method="post">
-		<fieldset class="form_modif">
-			<label>
-				Nom
-				<input
-					name="name"
-					placeholder="Nom du produit"
-					autocomplete="name"
-					value="<?= $article['name'] ?>" />
-			</label>
-			<label>
-				Catégorie
-				<input
-					type="number"
-					name="id_category"
-					placeholder="ID"
-					autocomplete="id_category"
-					value="<?= $article['id_category'] ?>" />
-			</label>
-			<label>
-				Description
-				<input
-					name="description"
-					placeholder="description"
-					autocomplete="description"
-					value="<?= $article['description'] ?>" />
-			</label>
-			<label>
-				illustration
-				<input
-					name="illustration"
-					placeholder="illustration"
-					autocomplete="illustration"
-					value="<?= $article['illustration'] ?>" />
-			</label>
-			<label>
-				new
-				<input
-					type="number"
-					name="is_new"
-					placeholder="ID"
-					autocomplete="is_new"
-					value="<?= $article['is_new'] ?>" />
-			</label>
-			<label>
-				promo
-				<input
-					type="number"
-					name="is_promo"
-					placeholder="ID"
-					autocomplete="is_promo"
-					value="<?= $article['is_promo'] ?>" />
-			</label>
-			<label>
-				Prix
-				<input
-					type="number"
-					name="price"
-					placeholder="€"
-					autocomplete="price"
-					value="<?= $article['price'] ?>" />
-			</label>
+	<form method="post" class="form_modif">
+		<label>
+			Nom
 			<input
-			type="submit"
-			value="Mettre à jour" />
-		</fieldset>
+				name="name"
+				placeholder="Nom du produit"
+				autocomplete="name"
+				value="<?= $article['name'] ?>" />
+		</label>
+		<label>
+			Catégorie
+			<input
+				type="number"
+				name="id_category"
+				placeholder="ID"
+				autocomplete="id_category"
+				value="<?= $article['id_category'] ?>" />
+		</label>
+		<label>
+			Description
+			<input
+				name="description"
+				placeholder="description"
+				autocomplete="description"
+				value="<?= $article['description'] ?>" />
+		</label>
+		<label>
+			illustration
+			<input
+				name="illustration"
+				placeholder="illustration"
+				autocomplete="illustration"
+				value="<?= $article['illustration'] ?>" />
+		</label>
+		<label>
+			new
+			<input
+				type="number"
+				name="is_new"
+				placeholder="ID"
+				autocomplete="is_new"
+				value="<?= $article['is_new'] ?>" />
+		</label>
+		<label>
+			promo
+			<input
+				type="number"
+				name="is_promo"
+				placeholder="ID"
+				autocomplete="is_promo"
+				value="<?= $article['is_promo'] ?>" />
+		</label>
+		<label>
+			Prix
+			<input
+				type="number"
+				name="price"
+				placeholder="€"
+				autocomplete="price"
+				value="<?= $article['price'] ?>" />
+		</label>
+		<button>Modifier</button>
 	</form>
 
-	<form method="post">
+	<form method="post" class="form_modif">
 		<input type="hidden" name="id" value="<?= $_GET['id'] ?>">
-		<input
-			type="submit"
-			class="outline secondary"
-			value="Supprimer l'article" />
+		<button>Supprimer</button>
 	</form>

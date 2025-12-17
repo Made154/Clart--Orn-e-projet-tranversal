ddb2fe258ddeddfc223b2b6fc47e70692f50bec1
Achyl Janot l'élève aux pensé seraine <lychal0411@gmail.com>
2025-12-17 03:33:57 +0100

    enlève commentaire

--- models/deconnection.php
+++ models/deconnection.php
@@ -1,12 +1,9 @@
 <?php
 session_start();
 
-// Détruit toutes les variables de session
 $_SESSION = [];
 
-// Détruit la session
 session_destroy();
 
-// Redirection vers la page de connexion ou accueil
 header("Location: ../index.php?page=connection");
 exit;
